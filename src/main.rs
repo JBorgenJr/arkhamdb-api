34e63e8442537883a1c0bd2f9de23912e2624e32
Jon <jon.borgenjr@tanduo.io>
2023-02-21 18:09:38 -0600

    updated API to allow for passing in args

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use std::fmt::format;
-
 use reqwest;
+use std::env;
 
+// Retrieve all cards from ArkhamDB
 async fn get_all_cards() -> Result<(), reqwest::Error> {
     let response = reqwest::get("https://arkhamdb.com/api/public/cards/")
         .await?
@@ -10,20 +10,30 @@ async fn get_all_cards() -> Result<(), reqwest::Error> {
     println!("{:#?}", response);
     Ok(())
 }
+
+// Retrieve single card from ArkhamDB
 async fn get_card(value: &str) -> Result<(), reqwest::Error> {
     let url = format!("https://arkhamdb.com/api/public/card/{}", value);
     let response = reqwest::get(&url)
         .await?
         .json::<serde_json::Value>()
         .await?;
-    println!("{:#?}", url);
     println!("{:#?}", response);
     Ok(())
 }
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
-    // get_all_cards().await?;
-    get_card("01001").await?;
+    // get arguments from terminal
+    let args: Vec<String> = env::args().collect();
+
+    // if there was a card code specified get that card else get all cards
+    if args.len() < 1 {
+        let value = &args[1];
+        get_card(value).await?;
+    } else {
+        get_all_cards().await?;
+    }
+
     Ok(())
 }
